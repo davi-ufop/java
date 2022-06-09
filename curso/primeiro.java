2c8ddc8f15520cedeb04489da3b53d50b0bbd1bf
davi-ufop <davi.ufop@gmail.com>
2022-06-09 13:25:23 -0300

    java_#1

---
+++ curso/primeiro.java
@@ -0,0 +1,15 @@
+// Meu primeiro programa em Java -> Oi mundo!
+// Davi Neves - Ouro Preto/MG - 06/2022
+
+// Definindo a classe do código - essencial
+class primeira {
+  // Definindo o método do código - essencial
+  public static void main (String args[]) {
+    // Definindo o comando de 'print' na tela - funcional
+    System.out.println("Oi mundo!");
+  } // Fecha o método
+} // Fecha a classe
+// FIM do código
+
+// Pra compilar: javac primeiro.java
+// Pra executar: java primeira
