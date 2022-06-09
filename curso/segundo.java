a4534263991aee846902e79f53f01bb2b330e7b8
davi-ufop <davi.ufop@gmail.com>
2022-06-09 14:25:59 -0300

    java_#2

---
+++ curso/segundo.java
@@ -0,0 +1,19 @@
+// Segundo programa Java, aprendendo a lidar com variáveis
+// Davi Neves - Ouro Preto, MG, Brasil - 06/2022
+
+// Definindo a classe
+class segunda {
+  // Definindo o método principal
+  public static void main (String args[]) {
+    // Variável do tipo string
+    String a;
+    // Atributo para a variável
+    a = "Oi mundo!";
+    // Apresentando a String na tela
+    System.out.println(a);
+  }
+}
+// FIM
+
+// Pra compilar: javac segundo.java
+// Pra executar: java segunda
